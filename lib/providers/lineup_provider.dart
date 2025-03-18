c0157cb4d505d19f320aaf0b15792ee98a6c026e
Nils Janson <nils.janson@callistaenterprise.se>
2025-03-18 09:20:39 +0100

    feat: select players for possition

---
+++ lib/providers/lineup_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:subsub/models/player.dart';
+import 'package:subsub/models/position.dart';
+
+class LineupNotifier extends StateNotifier<Map<String, Player?>> {
+  LineupNotifier() : super({});
+
+  void assignPlayer(String positionId, Player? player) {
+    state = {...state, positionId: player};
+  }
+
+  void removePlayer(String positionId) {
+    state = {...state, positionId: null};
+  }
+
+  Player? getPlayerAtPosition(String positionId) {
+    return state[positionId];
+  }
+
+  bool isPlayerAssigned(Player player) {
+    return state.values.any((p) => p?.id == player.id);
+  }
+
+  void clearAllAssignments() {
+    state = {};
+  }
+}
+
+final lineupProvider = StateNotifierProvider<LineupNotifier, Map<String, Player?>>((ref) {
+  return LineupNotifier();
+}); 
